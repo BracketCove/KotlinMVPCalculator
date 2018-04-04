c42eee2f1447d170307bb2004c07fda6a59ef44b
r_kay <rkay301@gmail.com>
2018-04-04 10:39:39 -0700

    starting point

--- app/src/main/java/com/wiseassblog/kotlincalculator/data/datamodel/ExpressionDataModel.kt
+++
@@ -1,7 +0,0 @@
-package com.wiseassblog.kotlincalculator.data.datamodel
-
-/**
- * Created by R_KAY on 1/20/2018.
- */
-data class ExpressionDataModel(val value:String,
-                               val isValid:Boolean)
--- app/src/main/java/com/wiseassblog/kotlincalculator/data/datamodel/OperandDataModel.kt
+++
@@ -1,6 +0,0 @@
-package com.wiseassblog.kotlincalculator.data.datamodel
-
-/**
- * Created by R_KAY on 1/17/2018.
- */
-data class OperandDataModel(val value:String)
